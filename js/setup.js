b52800fc87407cf4387bfb7cb4f7f81bf2a70ce5
flyfial <30seconds_to_me@mail.ru>
2017-02-16 22:49:19 +0900

    Color changes with click

--- js/setup.js
+++ js/setup.js
@@ -4,11 +4,11 @@ var setup = document.querySelector('.setup');
 var setupOpen = document.querySelector('.setup-open');
 var setupClose = document.querySelector('.setup-close');
 
-setupOpen.addEventListener('click', function() {
+setupOpen.addEventListener('click', function () {
   setup.classList.remove('invisible');
 });
 
-setupClose.addEventListener('click', function() {
+setupClose.addEventListener('click', function () {
   setup.classList.add('invisible');
 });
 
@@ -16,42 +16,43 @@ var username = setup.querySelector('.setup-user-name');
 username.required = true;
 username.maxlength = 50;
 
-var getRandomElement = function(array) {
+var getRandomElement = function (array) {
   var randomElementIndex = Math.floor(Math.random() * array.length);
   return array[randomElementIndex];
 };
 
+var wizard = document.querySelector('#wizard');
 var wizardCoat = wizard.querySelector('#wizard-coat');
 var wizardEyes = wizard.querySelector('#wizard-eyes');
 var fireballWrap = document.querySelector('.setup-fireball-wrap');
 var wizardCoatColors = [
-'rgb(101, 137, 164)',
-'rgb(241, 43, 107)',
-'rgb(146, 100, 161)',
-'rgb(56, 159, 117)',
-'rgb(215, 210, 55)',
-'rgb(0, 0, 0)'];
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'];
 var wizardEyesColors = [
-'black',
-'red',
-'blue',
-'yellow',
-'green'];
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'];
 var fireballWrapColors = [
-'#ee4830',
-'#30a8ee',
-'#5ce6c0',
-'#e848d5',
-'#e6e848'];
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'];
 
-wizardCoat.addEventListener('click', function() {
+wizardCoat.addEventListener('click', function () {
   wizardCoat.style.fill = getRandomElement(wizardCoatColors);
 });
 
-wizardEyes.addEventListener('click', function() {
+wizardEyes.addEventListener('click', function () {
   wizardEyes.style.fill = getRandomElement(wizardEyesColors);
 });
 
-fireballWrap.addEventListener('click', function() {
+fireballWrap.addEventListener('click', function () {
   fireballWrap.style.backgroundColor = getRandomElement(fireballWrapColors);
 });
